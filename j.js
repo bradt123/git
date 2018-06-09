bdda27d24eb0eb62acab05624a77ac77e37ce604
bradt123 <bradtbrey1234@gmail.com>
2018-06-09 00:17:37 -0400

    prueba1_git

--- j.js
+++ j.js
@@ -1,7 +1,4 @@
-change=function(btn,color){
-if (btn.style.backgroundColor == "#f47121") {
-        btn.style.backgroundColor = color;
-    } else {
-        btn.style.backgroundColor = "#f47121";
-    }
+change=function(){
+	let color = document.getElementById('b');
+	color.backgroundColor = 'red';
 }
